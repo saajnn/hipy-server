918468cbca80135371846577ca1ba1c65dff92b9
晚风拂柳颜 <434857005@qq.com>
2024-05-16 15:51:39 +0800

    新增一些源。增加一套mx模板

--- app/t4/qjs_drpy/qjs_module_muban.js
+++ app/t4/qjs_drpy/qjs_module_muban.js
@@ -15,6 +15,34 @@ if (typeof Object.assign != 'function') {
 
 function getMubans() {
     var mubanDict = { // 模板字典
+        mx: {
+            title: '',
+            host: '',
+            url: '/vodshow/fyclass--------fypage---/',
+            searchUrl: '/vodsearch/**----------fypage---/',
+            class_parse: '.top_nav li;a&&Text;a&&href;.*/(.*?)/',
+            searchable: 2,
+            quickSearch: 0,
+            filterable: 0,
+            headers: {
+                'User-Agent': 'MOBILE_UA',
+            },
+            play_parse: true,
+            lazy: '',
+            limit: 6,
+            推荐: '.cbox_list;*;*;*;*;*',
+            double: true,
+            一级: 'ul.vodlist li;a&&title;a&&data-original;.pic_text&&Text;a&&href',
+            二级: {
+                title: 'h2&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(2)&&Text',
+                img: '.vodlist_thumb&&data-original',
+                desc: '.content_detail&&li:eq(1)&&Text;.detail_list&&ul:eq(1)&&li&&a&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(1)&&Text;.detail_list&&ul:eq(1)&&li:eq(2)&&Text;.detail_list&&ul:eq(1)&&li:eq(3)&&Text',
+                content: '.content_desc&&span&&Text',
+                tabs: '.play_source_tab&&a',
+                lists: '.content_playlist:eq(#id) li',
+            },
+            搜索: '*',
+        },
         mxpro: {
             title: '',
             host: '',
