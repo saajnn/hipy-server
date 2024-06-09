209e29671e96ef494af97afc5cad0ca082f82e82
晚风拂柳颜 <434857005@qq.com>
2024-06-10 00:25:21 +0800

    更新几个源

---
+++ app/t4/files/drpy_js/大米动漫.js
@@ -0,0 +1,8 @@
+var rule = {
+    模板: 'mxpro',
+    title: '大米动漫',
+    host: 'https://damidm.com/',
+    url: 'show/fyclass--------fypage---.html',
+    searchUrl: '/search/**----------2---.html',
+    class_parse: '.navbar-items li;a&&Text;a&&href;/(\\d+).html',
+}
