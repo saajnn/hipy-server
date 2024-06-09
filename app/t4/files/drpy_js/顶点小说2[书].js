13e1d265bc241d14fb497af55efbb8bcdb65105b
晚风拂柳颜 <434857005@qq.com>
2024-06-10 02:52:18 +0800

    新增源

--- app/t4/files/drpy_js/顶点小说2[书].js
+++ app/t4/files/drpy_js/顶点小说2[书].js
@@ -1,11 +1,11 @@
 var rule = {
     类型: '小说',//影视|听书|漫画|小说
-    title: '顶点小说[书]',
-    host: 'https://www.23ddw.cc/',
+    title: '顶点小说2[书]',
+    host: 'https://www.ddyueshu.cc/',
     编码: 'gb18030',
     url: '/fyclass/#fypage',
 
-    searchUrl: '/modules/article/search.php?searchkey=**&page=fypage',
+    searchUrl: '/xiaoshuodaquan/#key=',
     searchable: 2,
     quickSearch: 0,
     filterable: 1,
@@ -16,15 +16,15 @@ var rule = {
         'User-Agent': 'MOBILE_UA',
     },
     timeout: 5000,
-    class_name: '全本',
-    class_url: '0',
-    class_parse: '.nav&&ul&&li;a&&Text;a&&href;class/(.*?)_',
-    cate_exclude: '',
+    //class_name: '全本',
+    //class_url: '0',
+    class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?)/',
+    cate_exclude: '书架|排行',
     play_parse: true,
     lazy: $js.toString(() => {
         let html = request(input);
         let title = pdfh(html, '.bookname&&h1&&Text');
-        let content = pdfh(html, '#content&&Html').replace(/\n/g, "").split("<br>").filter(v => v).slice(0).join("\n").replace(/&nbsp;/g, ' ');
+        let content = pdfh(html, '#content&&Html').replace(/\n/g, "").split("<br>").filter(v => v).slice(0, -2).join("\n").replace(/&nbsp;/g, ' ');
         let ret = JSON.stringify({
             title,
             content
@@ -49,11 +49,11 @@ var rule = {
             });
         } else {
             let html = request(input.split('#')[0]);
-            let lis = pdfa(html, '#newscontent&&ul&&li');
+            let lis = pdfa(html, '.up&&ul&&li');
             lis.forEach(it => {
                 d.push({
                     title: pdfh(it, '.s2&&Text'),
-                    desc: pdfh(it, '.s5&&Text'),
+                    desc: pdfh(it, '.s4&&Text'),
                     img: "",
                     url: pd(it, 'a&&href', MY_URL),
                 });
@@ -66,12 +66,27 @@ var rule = {
         img: '#fmimg&&img&&src',
         desc: '#info&&p:eq(-1)&&Text',
         content: '#intro&&p&&Text',
-        tabs: '#list&&dt',
+        tabs: '#list&&dt&&b',
         lists: '#list&&dd',
-        tab_text: 'body&&Text',
+        tab_text: 'b&&Text',
         list_text: 'body&&Text',
         list_url: 'a&&href',
         list_url_prefix: '',
     },
-    搜索: 'table.grid&&tr:gt(0);a&&Text;;.odd:eq(1)&&Text;a&&href;a:eq(1)&&Text',
+    搜索: $js.toString(() => {
+        let html = request(input.split('#')[0]);
+        let key = decodeStr(KEY, rule.编码);
+        //log(key);
+        let lis = pdfa(html, '.novellist&&ul&&li').filter(it => it.includes(key));
+        let d = [];
+        lis.forEach(it => {
+            d.push({
+                title: pdfh(it, 'a&&Text'),
+                desc: "",
+                img: "",
+                url: pd(it, 'a&&href', MY_URL),
+            })
+        });
+        setResult(d);
+    }),
 }
