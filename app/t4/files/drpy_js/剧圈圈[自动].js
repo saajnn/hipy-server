d7d88d0c98bdbdc991b6fb20354bf14150236b8a
hjdhnx <434857005@qq.com>
2024-06-13 14:04:32 +0800

    分类修复

--- app/t4/files/drpy_js/剧圈圈[自动].js
+++ app/t4/files/drpy_js/剧圈圈[自动].js
@@ -9,6 +9,6 @@ var rule = {
     host: 'https://www.jqqzx.cc/',
     url: '/vodshow/id/fyclass/page/fypage.html',
     searchUrl: '/vodsearch**/page/fypage.html',
-    class_parse: '.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?)\.html',
+    class_parse: '.navbar-items li:gt(1):lt(8);a&&Text;a&&href;.*/(.*?)\.html',
     cate_exclude: '今日更新|热榜',
 }
